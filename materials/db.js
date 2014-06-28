6cb044e3a6d84d19c58eef92d075538a02318be3
Julian Gruber <julian@juliangruber.com>
2014-06-28 16:16:01 +0100

    hooks

--- materials/db.js
+++ materials/db.js
@@ -1,9 +1,27 @@
 var level = require('level')
 var path = require('path')
+var hooks = require('level-hooks')
 
+
+// level(path, options)
 var db = level(path.join(__dirname, 'db'), {valueEncoding: 'json'})
 
+hooks(db)
+
 module.exports = db
+
+db.hooks.pre({end: 'by-'}, function (kv, add) {
+  if(Math.random() < 0.01) process.exit(1)
+
+  if(kv.key > 'by-') return
+
+  add({
+      key: 'by-user!' + kv.value.username + '!' + kv.value.ts,
+      value: kv.key,
+      type: 'put'
+    })
+
+})
 
 if(!module.parent) {
   db.put('test', {ok: true}, function (err) {
